e8114c57967c5cd2a82b37831aa6cf086fb8b01d
Tanmay Singh <157793653+tanmay-563@users.noreply.github.com>
2025-01-16 03:17:35 +0530

    Update Stack-linkedlist.c

--- Stack-linkedlist.c
+++ Stack-linkedlist.c
@@ -51,14 +51,51 @@ int pop(struct node**top){
         return x;
     }
 }
+int peek(int pos, struct node*top){
+    struct node*ptr = top;
+    for(int i =0; i<pos-1 && ptr != NULL; i++){
+        ptr= ptr->next;
+    }
+    if(ptr!=NULL){
+        return ptr->data;
+    }
+    else{
+        return -1;
+    }
+}
+int stacktop(struct node*top){
+    if(empty(top)){
+        return -1;
+    }
+    else{
+        printf("the top element of stack is %d\n", top->data);
+        }
+}
+int stackbottom(struct node*top){
+    struct node*ptr = top;
+    while(ptr->next!= NULL){
+        ptr = ptr->next;
+    }
+    printf("the last element of stack is %d\n", ptr->data);
+}
 int main(){
     struct node*top = NULL;
     top = push(78, top);
     top = push(98, top);
     top = push(43, top);
-    top = push(25, top);
+    top = push(77, top);
+    top = push(90, top);
+    top = push(61, top);
+    top = push(67, top);
+    top = push(97, top);
+    int elemnt =pop(&top);
+    printf("element removed %d\n", elemnt);
     int element =pop(&top);
     printf("element removed %d\n", element);
     traversal(top);
+    int k = peek(3,top);
+    printf("The peeked element is %d\n", k);
+    stackbottom(top);
+    stacktop(top);
     return 0;
 }
